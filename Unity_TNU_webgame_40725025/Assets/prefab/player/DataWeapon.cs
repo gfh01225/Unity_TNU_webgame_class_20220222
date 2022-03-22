479ce32138f3e2ec5d732736ac529bf0bf0d02bf
gfh01225 <100176011+gfh01225@users.noreply.github.com>
2022-03-22 12:00:30 +0800

    武器腳本創建

---
+++ Unity_TNU_webgame_40725025/Assets/prefab/player/DataWeapon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace g
+{
+    [CreateAssetMenu(menuName = "g/Data Weapon",fileName ="data weapon")]
+    public class DataWeapon : ScriptableObject
+    {
+
+
+    }
+
+}
